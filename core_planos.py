93d9ff81408384d4f4839f5bb4751783ac04b3e2
Manuelenriquep <44738536+Manuelenriquep@users.noreply.github.com>
2026-02-02 13:08:38 -0500

    Update core_planos.py

--- core_planos.py
+++ core_planos.py
@@ -1,61 +1,56 @@
 import math
 
 class CoreFerrotek:
-    """
-    Núcleo de Ingeniería Ferrotek v56.0
-    Basado en 20 años de investigación en morteros y estructuras Unibody.
-    """
-    
-    def __init__(self, precios):
-        self.p = precios # Recibe el diccionario de precios desde la DB
+    def __init__(self, precios, margen_utilidad):
+        self.p = precios
+        self.MARGEN = 1 - margen_utilidad
         self.H_VISTA = 2.20
         self.H_MALLA = 2.35
-        self.ZAPATA_RAIZ = 0.15 # 15cm excedentes para enterrar
 
-    def calcular_cerramiento_perimetral(self, ml):
-        """Muro de Cerramiento con Sistema Raíz y Matriz 1:3:3"""
-        # Estructura: Postes de 2" cada 1.50m
+    def calcular_muro_perimetral(self, ml):
+        """Muro 2.20m - Sistema Raíz con Zapata Continua"""
         cant_postes = math.ceil(ml / 1.5) + 1
-        mts_perfil = cant_postes * (self.H_VISTA + 0.6) # Incluye dado de concreto
-        costo_acero = mts_perfil * self.p['perfil_2_pulg_mt']
-        
-        # Blindaje: Malla 5mm + Zaranda (Simple Membrana para cerramiento)
-        area_malla = ml * self.H_MALLA
-        costo_mallas = (area_malla * self.p['malla_5mm_m2']) + (area_malla * 2 * self.p['malla_zaranda_m2'])
-        
-        # Matriz: 1 Cemento, 3 Arena, 3 Cal (4cm espesor)
-        vol_mezcla = (ml * self.H_VISTA * 0.04) * 1.10 # 10% desperdicio
-        costo_mezcla = vol_mezcla * (5 * self.p['cemento_bulto'] + 5 * self.p['cal_bulto'] + self.p['arena_m3'])
-        
-        # Mano de Obra: Rendimiento industrializado
-        costo_mo = (ml * 0.8) * self.p['valor_jornal']
-        
-        directo = costo_acero + costo_mallas + costo_mezcla + costo_mo
-        return {"total": directo, "ml": directo/ml, "postes": cant_postes}
+        acero = (cant_postes * 2.8 * self.p['perfil_2_pulg_mt']) + (ml * 3 * 8500)
+        area = ml * self.H_MALLA
+        mallas = (area * self.p['malla_5mm_m2']) + (area * 2 * self.p['malla_zaranda_m2'])
+        mezcla = (ml * self.H_VISTA * 0.04 * 1.1) * (5*self.p['cemento_bulto'] + 5*self.p['cal_bulto'] + self.p['arena_m3'])
+        costo_dir = acero + mallas + mezcla + (ml * 0.85 * self.p['valor_jornal'])
+        return round(costo_dir / self.MARGEN, -3)
 
-    def calcular_muros_vivienda(self, perimetro_ext, divisiones_int):
-        """Lógica Unificada: Fachada (Doble Membrana) vs Internos (Simple)"""
-        
-        # 1. FACHADAS (Doble Membrana - Confort Térmico)
-        area_ext = perimetro_ext * self.H_VISTA
-        mallas_ext = area_ext * (self.p['malla_5mm_m2'] + 2 * self.p['malla_zaranda_m2'])
-        mortero_ext = (area_ext * 0.05) * (5 * self.p['cemento_bulto'] + 5 * self.p['cal_bulto'] + self.p['arena_m3'])
-        
-        # 2. INTERNOS (Membrana Simple - Ganancia de Área)
-        area_int = divisiones_int * self.H_VISTA
+    def calcular_vivienda_unibody(self, area_m2):
+        """Casas: Fachada Doble / Internos Simple (Matriz 1:3:3)"""
+        perim = math.sqrt(area_m2) * 4
+        div_int = perim * 0.6
+        area_ext = perim * 2.4
+        mallas_ext = area_ext * (self.p['malla_5mm_m2'] + 2*self.p['malla_zaranda_m2'])
+        area_int = div_int * 2.4
         mallas_int = area_int * (self.p['malla_5mm_m2'] + self.p['malla_zaranda_m2'])
-        mortero_int = (area_int * 0.035) * (5 * self.p['cemento_bulto'] + 5 * self.p['cal_bulto'] + self.p['arena_m3'])
-        
-        # 3. ESTRUCTURA COMPLETA (Postes 2" @ 1.5m)
-        cant_postes = math.ceil((perimetro_ext + divisiones_int) / 1.5)
-        costo_acero = (cant_postes * 3.0) * self.p['perfil_2_pulg_mt']
-        
-        total_muros = mallas_ext + mortero_ext + mallas_int + mortero_int + costo_acero
-        return total_muros
+        mezcla_muros = (area_ext + area_int) * 0.04 * (5*self.p['cemento_bulto'] + 5*self.p['cal_bulto'] + self.p['arena_m3'])
+        pisos_tech = area_m2 * (0.2*self.p['aditivo_F1_kg'] + 0.1*self.p['sellado_FX_galon'])
+        estructura = (perim + div_int) * 1.5 * self.p['perfil_2_pulg_mt']
+        techo = area_m2 * 85000 
+        costo_dir = mallas_ext + mallas_int + mezcla_muros + pisos_tech + estructura + techo + (area_m2 * 2.8 * self.p['valor_jornal'])
+        return round(costo_dir / self.MARGEN, -3)
 
-    def calcular_piso_polimerico(self, area_m2):
-        """Matriz 2:1 con Polímeros y Sellado en Poliuretano"""
-        # Cemento rico + Aditivo F1 + Sellado FX
-        costo_quimicos = area_m2 * (0.25 * self.p['aditivo_F1_kg'] + 0.12 * self.p['sellado_FX_galon'])
-        vol_mezcla = (area_m2 * 0.05) * (10 * self.p['cemento_bulto'] + self.p['arena_m3'])
-        return costo_quimicos + vol_mezcla
+    def calcular_boveda_v58(self, largo_m):
+        """Bóveda Ferrotek: Base Perfil C18 + Arcos de Varilla (3.80m x 2.40m)"""
+        ancho = 3.80
+        altura = 2.40
+        area_suelo = ancho * largo_m
+        perimetro_base = (ancho + largo_m) * 2
+        
+        # 1. Base Guía: Perfil C Calibre 18 (Primeros 90cm de altura)
+        costo_perfil_c = perimetro_base * 0.9 * 11500 # Valor est. perfil C18
+        
+        # 2. Arcos: Varilla corrugada cada 0.60m naciente del Perfil C
+        num_arcos = math.ceil(largo_m / 0.6)
+        longitud_arco = 5.20 # Longitud curva aprox para 3.80m de luz y 2.40m alto
+        acero_arcos = (num_arcos * longitud_arco) * 8500 # Precio varilla
+        
+        # 3. Piel: Malla 5mm + Zaranda Doble + Mezcla 1:3:3
+        area_curva = (longitud_arco * largo_m)
+        mallas = area_curva * (self.p['malla_5mm_m2'] + 2*self.p['malla_zaranda_m2'])
+        mezcla = area_curva * 0.045 * (5*self.p['cemento_bulto'] + 5*self.p['cal_bulto'] + self.p['arena_m3'])
+        
+        costo_dir = costo_perfil_c + acero_arcos + mallas + mezcla + (20 * self.p['valor_jornal'])
+        return round(costo_dir / self.MARGEN, -3)
